f0ac1e5169361cf76fe4c0df9acd71ec347f614d
wangziyi <1299194997@qq.com>
2020-06-10 16:34:16 +0800

    完善reducer逻辑

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,14 @@
-export default function setMusic(state=[],action){
-   switch(action.type){
-         case 'ADD_MUSIC':
-           return [...state,...action.payload]
-           break
-         case 'DELETE_MUISIC':
-           return [...state].splice(action.payload,1)
-           break
-         default:
-           break
-   }
+export default function setMusic(state = [], action) {
+  let newState
+  switch (action.type) {
+    case 'ADD_MUSIC':
+      newState = [...state, ...action.payload]
+      break
+    case 'DELETE_MUISIC':
+      newState = state.splice(action.payload, 1)
+      break
+    default:
+      break
+  }
+  return newState
 }
