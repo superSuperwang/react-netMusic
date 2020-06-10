5942238ead4fc2b9cd4b6628ec1f677e3cbd4df6
wangziyi <1299194997@qq.com>
2020-06-10 16:29:33 +0800

    创建播放音乐组件

---
+++ src/components/PlayMusic/PlayMusic.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './playMusic.scss'
+
+class PlayMusic extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default PlayMusic;
