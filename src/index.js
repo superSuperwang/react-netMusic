a59a2e4daa25b2ad1ff4e873520bf90c8385bf60
wangziyi <1299194997@qq.com>
2020-06-10 18:55:20 +0800

    删除无用文件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/swiper.css'
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom"
